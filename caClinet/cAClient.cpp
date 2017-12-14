44ac89bc8f627242a6466844096413936a8b8f86
root <root@ip-172-31-2-68.ec2.internal>
2017-12-14 14:05:23 +0000

    conf file

--- caClinet/cAClient.cpp
+++ caClinet/cAClient.cpp
@@ -5,6 +5,8 @@
 using namespace std;
 
 
+   char url[100];
+   char fileloc[100];
 //copy the value return from the HTTP Get to the memorty
 size_t WriteCallback(char *contents, size_t size, size_t nmemb, void *userp)
 {
@@ -21,15 +23,16 @@ int requestCert(std::string   hostname)
 
     CURL* easyhandle = curl_easy_init();  /* init the curl session */ 
     std::string readBuffer;
-    std::string url;
+    std::string serverurl;
    
     const char *data = "data to send";
+  
+    serverurl=url;
 
    //init the Url -For Create Ca Request 
-    url="http://52.55.95.135:50026/api/Cert/Createreq?hostname=" + hostname;
-    
-    /* specify URL to get */ 
-    curl_easy_setopt(easyhandle, CURLOPT_URL,url.c_str()); 
+    serverurl+= "/Createreq?hostname=" + hostname;
+      /* specify URL to get */ 
+    curl_easy_setopt(easyhandle, CURLOPT_URL,serverurl.c_str()); 
 
     /* Create Post Request */ 
     curl_easy_setopt(easyhandle, CURLOPT_POSTFIELDS,data);
@@ -46,6 +49,34 @@ int requestCert(std::string   hostname)
     return reqid;
 }
 
+int  getRuestStatus(int   reqid)
+{
+ curl_global_init(CURL_GLOBAL_ALL);
+
+    CURL* easyhandle = curl_easy_init(); /* init the curl session */ 
+    std::string readBuffer;
+    std::string serverurl;
+    int status;
+
+    serverurl=url;
+    /*init the Url -For Getting the  Ca */
+    serverurl+="/GetStatus?reqid="+std::to_string(reqid);    
+    
+    /* specify URL to get */ 
+    curl_easy_setopt(easyhandle, CURLOPT_URL,serverurl.c_str()); 
+    /* send all data to this function  to save the Return Value */  
+    curl_easy_setopt(easyhandle, CURLOPT_WRITEFUNCTION, WriteCallback);    
+    curl_easy_setopt(easyhandle, CURLOPT_WRITEDATA, &readBuffer);
+
+    /*Perform Http Rest Get */
+    curl_easy_perform(easyhandle);
+   // cout<<readBuffer.c_str()<<endl;
+    status=std::atoi(readBuffer.c_str());   
+    return status;
+    
+}
+
+
 /*Function to Return the Certificate That Was Issued by Microsoft CA */
 void getCertificate(int   reqid)
 {
@@ -53,21 +84,22 @@ void getCertificate(int   reqid)
 
     CURL* easyhandle = curl_easy_init(); /* init the curl session */ 
     std::string readBuffer;
-    std::string url;
-       
+    std::string serverurl;
     /*init the Url -For Getting the  Ca */
-    url="http://52.55.95.135:50026/api/Cert/GetCert?reqid="+std::to_string(reqid);    
+       serverurl=url;
+    serverurl+="/GetCert?reqid="+std::to_string(reqid);    
+    
     /* specify URL to get */ 
-    curl_easy_setopt(easyhandle, CURLOPT_URL,url.c_str()); 
+    curl_easy_setopt(easyhandle, CURLOPT_URL,serverurl.c_str()); 
     /* send all data to this function  to save the Return Value */  
     curl_easy_setopt(easyhandle, CURLOPT_WRITEFUNCTION, WriteCallback);    
     curl_easy_setopt(easyhandle, CURLOPT_WRITEDATA, &readBuffer);
 
     /*Perform Http Rest Get */
     curl_easy_perform(easyhandle);
-
+  
     /*open A file to Write -- cert.cert*/
-    std::ofstream out("cert.cer");
+    std::ofstream out(fileloc);
 
     /*Write The Certificate To the File*/
     out << readBuffer.c_str();
@@ -76,11 +108,37 @@ void getCertificate(int   reqid)
     
 }
 
+
+
+
 int main(int argc, char * argv[])
 {
-    int reqid;
-    
+  int reqid;
+  int  status;
+ 
+  
+
+  ifstream in("conf.txt");
+  if(!in) {
+    cout << "Cannot open input file.\n";
+    return 1;
+  }
+  
+    in.getline(url, 100); 
+    in.getline(fileloc, 100); 
+    in.close();
     reqid=requestCert(argv[1]);
-    getCertificate(reqid);
+    status=getRuestStatus(reqid);
+  
+    if (status==3) 
+    {
+        getCertificate(reqid);
+    }
+
+    else
+    {
+        cout<<"The Certificate Is Yet Not Issued"<<endl;
+    }
+
     return 0;
 }
