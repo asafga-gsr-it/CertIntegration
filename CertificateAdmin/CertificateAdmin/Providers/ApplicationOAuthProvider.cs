468f6243e12e98147c7e60fb37f490849b9924db
asafga-gsr-it <32237764+asafga-gsr-it@users.noreply.github.com>
2018-04-03 11:49:16 +0300

    validity Check For Host

--- CertificateAdmin/CertificateAdmin/Providers/ApplicationOAuthProvider.cs
+++ CertificateAdmin/CertificateAdmin/Providers/ApplicationOAuthProvider.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
@@ -42,22 +44,37 @@ namespace CertificateAdmin.Providers
             return Task.FromResult<object>(null);
 
         }
-     
 
 
+        public string CheckValidityMachine(string clientId,string Hash)
+        {
+
+            string url = "https://linuxinfra.wdf.sap.corp/ldt/webapp/production/ldt-get-signature.php?uuid=$HOST_UUID&hash=$hash";
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.ServerCertificateValidationCallback += (sender, certificate, chain, sslPolicyErrors) => true;
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            Stream stream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(stream);
+            return reader.ReadToEnd();
+        }
 
         public override Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
         {
             string clientId;
             string clientSecret;
+            string validity;
             context.TryGetFormCredentials(out clientId, out clientSecret);
-           // SqlLite sql = new SqlLite();
+            validity = CheckValidityMachine(clientId, clientSecret);
 
-            if (clientId==null) //& sql.checkClientWithHash(clientSecret, clientId))
+            if (validity == "Error: LDT Deployment is Not Valid!!") 
             {
                 context.SetError("invalid_grant", "Error The ClientId Is Not Recognize");
 
 
+            }
+            else if (validity == "Error: LDT Deployment is not Signed.")
+            {
+                context.SetError("invalid_grant", "Needed Premissions");
             }
             else
             {
